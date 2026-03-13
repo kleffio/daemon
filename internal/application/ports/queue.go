1e86c5f92902b5c721fee091fe7f4e878c6643eb
Jeefos <nathan.w.roos@gmail.com>
2026-03-13 17:07:41 -0400

    added queue interface port

---
+++ internal/application/ports/queue.go
@@ -0,0 +1,11 @@
+package ports
+
+import "github.com/kleffio/gameserver-daemon/internal/workers/jobs"
+
+type Queue interface {
+	Enqueue(job *jobs.Job) error
+	Dequeue() (*jobs.Job, error)
+	Acknowledge(jobID string) error
+	Retry(jobID string) error
+	MarkFailed(jobID string) error
+}
